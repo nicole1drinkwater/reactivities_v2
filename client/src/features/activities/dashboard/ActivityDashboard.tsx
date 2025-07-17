3e7f99824fe2958e89bd8e210ff16be063db4bc6
nicole1drinkwater <nicnacsdrinkwater@gmail.com>
2025-07-17 11:52:22 +0200

    End of Section 5

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -11,16 +11,20 @@ type Props = {
     openForm: (id: string) => void;
     closeForm: () => void;
     editMode: boolean;
+    submitForm: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityDashboard({activities, cancelSelectActivity, selectActivity, selectedActivity, openForm, closeForm, editMode}: Props) {
+export default function ActivityDashboard({activities, cancelSelectActivity, selectActivity, selectedActivity, openForm, closeForm, editMode, submitForm
+    , deleteActivity
+}: Props) {
     return (
         <Grid2 container spacing={3}>
             <Grid2 size={7}>
                 <ActivityList 
                 activities={activities} 
                 selectActivity={selectActivity}
-
+                deleteActivity={deleteActivity}
                 />
             </Grid2>
             <Grid2 size={5}>
@@ -30,7 +34,11 @@ export default function ActivityDashboard({activities, cancelSelectActivity, sel
                 openForm={openForm}
                 />}
                 {editMode && 
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} />}
+                <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity} 
+                    submitForm={submitForm}
+                    />}
             </Grid2>
         </Grid2>
     )
